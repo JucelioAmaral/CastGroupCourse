5434e733ed57b3de33019db660b752dab2a7ebe4
RogerioTostes <rogertotes@yahoo.com.br>
2020-08-25 18:26:21 -0300

    added CategoryRepositorie

--- CastGroupCourse.Infra/Data/Repositories/CategoryRepositorie.cs
+++ CastGroupCourse.Infra/Data/Repositories/CategoryRepositorie.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using CastGroupCourse.Core.CourseAgg.Entities;
+using CastGroupCourse.Core.CourseAgg.Interfaces.Repositories;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 
 namespace CastGroupCourse.Infra.Data.Repositories
 {
-    class CategoryRepositorie
+    public class CategoryRepositorie : ICategory
     {
+        private readonly IDbConnection conexao;
+        public CategoryRepositorie(IConfiguration conf) => conexao = new SqlConnection("Data Source=localhost,1433;Initial Catalog=;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;");
+
+        public List<Category> GetCategory()
+        {
+            return this.conexao.Query<Category>("SELECT * FROM Category").ToList();
+        }
+        public Category GetCategoryId(int Id)
+        {
+            return this.conexao.Query<Category>("SELECT * FROM Category WHERE @Id = Id", new { Id = Id }).SingleOrDefault();
+        }
+        public void InsertCategory(Category Description)
+        {
+            this.conexao.Execute(@"INSERT INTO Category(Description,Id) VALUES (@Description,@Id)",
+             new { Description = Description.Description, Id = Description.Id });
+        }
+        public void DeleteCategory(int Id)
+        {
+            this.conexao.Execute(@"DELETE FROM Category WHERE @Id = id", new { Id = Id });
+        }
+        public void UpdateCategory(Category Description)
+        {
+            this.conexao.Execute("UPDATE Category SET Description=@Descriptiong WHERE Id=" + Description.Id,
+           new { Description = Description.Description });
+        }
     }
 }
